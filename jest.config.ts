cbe4b9ab5d480048d2ca830baf09b37d44c802fa
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-09-09 11:48:28 -0500

    +85% code coverage threshold

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,12 @@
 export default {
+	coverageThreshold: {
+		global: {
+			statements: 85,
+			branches: 85,
+			functions: 85,
+			lines: 85,
+		},
+	},
 	preset: 'ts-jest',
 	testEnvironment: 'jsdom',
 	setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
