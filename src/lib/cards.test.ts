63f9ae22bdc1125873602d520aa03b2e9704cbe9
fauxsaurus <31806063+fauxsaurus@users.noreply.github.com>
2025-09-09 11:25:53 -0500

    +`createDeck()` tests

---
+++ src/lib/cards.test.ts
@@ -0,0 +1,21 @@
+import {createDeck, shuffleDeck} from './cards'
+
+const deckLengthStandard = 52
+const deckLengthJoker = 54
+describe('Create Deck', () => {
+	const actualDeckStandard = createDeck()
+	const actualDeckJoker = createDeck(2)
+
+	it('Standard deck has 52 cards', () =>
+		expect(actualDeckStandard).toHaveLength(deckLengthStandard))
+
+	it('Joker deck has 54 cards', () => expect(actualDeckJoker).toHaveLength(deckLengthJoker))
+
+	it('Cards are in order', () => {
+		expect(actualDeckStandard[0]).toBe(0)
+		expect(actualDeckStandard.every((int, i, array) => (array[i - 1] ?? -1) + 1 === int)).toBe(
+			true
+		)
+	})
+})
+
